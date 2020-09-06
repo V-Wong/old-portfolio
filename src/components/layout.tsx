5d5b9036f5e3157d56676e177d6043d870351944
Vincent Wong <vincent@vwong.dev>
2020-09-06 19:44:10 +1000

    Add initial implementation of slider

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,10 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+
+import Slider from "react-slick";
+import useCurrentWidth from "./use-width";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Layout = ({ children }) => (
   <div className="container">
@@ -24,14 +30,27 @@ const TwoColumn = (props: {
   title: string,
   styles?: object,
   children: React.ReactNode
-}) => (
-  <>
-    <h2 className="section-heading">{props.title}</h2>
-    <hr/>
-    <section style={ props.styles } className="two-column">
-      {props.children}
-    </section>
-  </>
-)
+}) => {
+  const width = useCurrentWidth();
+  const [numSlides, setNumSlides] = useState(width);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    mobileFirst: true,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <>
+      <h2 className="section-heading">{props.title}</h2>
+      <hr/>
+      <Slider {...settings} slidesToShow={width >= 960 ? 2 : 1}>
+        {props.children}
+      </Slider>
+    </>
+  )
+}
 
 export { Layout, SingleColumn, TwoColumn }
