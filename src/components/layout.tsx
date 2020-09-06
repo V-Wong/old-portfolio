34a118f36717194bad4b9e3c4c6c87647661cb00
Vincent Wong <vincent@vwong.dev>
2020-09-07 00:07:24 +1000

    Add sliding carousel for projects (#2)
    * Add initial implementation of slider
    
    * Set slider to mobile only
    
    * Fix slider styling
    
    * Remove slide arrows

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,10 @@
-import React from "react"
+import React, { useRef, useEffect } from "react"
+
+import Slider from "react-slick";
+import useCurrentWidth from "./use-width";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Layout = ({ children }) => (
   <div className="container">
@@ -24,14 +30,41 @@ const TwoColumn = (props: {
   title: string,
   styles?: object,
   children: React.ReactNode
-}) => (
-  <>
-    <h2 className="section-heading">{props.title}</h2>
-    <hr/>
-    <section style={ props.styles } className="two-column">
-      {props.children}
-    </section>
-  </>
-)
+}) => {
+  const width = useCurrentWidth();
+  const trackRef = useRef(null);
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    arrows: false,
+    speed: 500,
+    mobileFirst: true,
+    slidesToScroll: 1,
+  };
+
+  if (width <= 960) {
+    return (
+      <>
+        <h2 className="section-heading">{props.title}</h2>
+        <hr/>
+        <Slider {...settings}>
+          {props.children}
+        </Slider>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h2 className="section-heading">{props.title}</h2>
+        <hr/>
+        <section style={ props.styles } className="two-column">
+          {props.children}
+        </section>
+      </>
+    );
+  }
+
+}
 
 export { Layout, SingleColumn, TwoColumn }
