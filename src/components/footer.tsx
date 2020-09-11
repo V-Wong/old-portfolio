08f2e31861e599fb024b37039d1fedf0d8d701ba
Vincent Wong <vincent@vwong.dev>
2020-09-11 17:30:05 +1000

    Refactor Footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,30 +1,34 @@
-import React from "react"
+import React from 'react';
 
-import githubImage from "../images/github.svg"
-import resumeImage from "../images/resume.svg"
-import linkedinImage from "../images/linkedin.svg"
+import githubImage from '../images/github.svg';
+import resumeImage from '../images/resume.svg';
+import linkedinImage from '../images/linkedin.svg';
 
 const Footer = () => (
   <footer>
-    <p>Looking for software roles. Email me at <a href="mailto:vincent@vwong.dev">vincent@vwong.dev</a> :)</p>
+    <p>
+      Looking for software roles. Email me at
+      <a href="mailto:vincent@vwong.dev">vincent@vwong.dev</a>
+      :)
+    </p>
     <div className="footer-links">
-        <div className="link-container">
-            <a href="https://github.com/V-Wong" target="_blank" rel="noopener noreferrer">
-              <img className="social-icon" src={githubImage} alt="Black and white GitHub logo icon"/>
-            </a>
-        </div>
-        <div className="link-container">
-            <a href="https://drive.google.com/file/d/1mqRF7QdBDXfDD7TLQ3OuanALxHxd_D2f/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-              <img className="social-icon" src={resumeImage} alt="Black and white icon with silhouette of person"/>
-            </a>
-        </div>
-        <div className="link-container">
-            <a href="https://www.linkedin.com/in/vincent-wc-wong/" target="_blank" rel="noopener noreferrer">
-              <img className="social-icon" src={linkedinImage} alt="Black and white LinkIn logo icon"/>
-            </a>
-        </div>
+      <div className="link-container">
+        <a href="https://github.com/V-Wong" target="_blank" rel="noopener noreferrer">
+          <img className="social-icon" src={githubImage} alt="Black and white GitHub logo icon" />
+        </a>
+      </div>
+      <div className="link-container">
+        <a href="https://drive.google.com/file/d/1mqRF7QdBDXfDD7TLQ3OuanALxHxd_D2f/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+          <img className="social-icon" src={resumeImage} alt="Black and white icon with silhouette of person" />
+        </a>
+      </div>
+      <div className="link-container">
+        <a href="https://www.linkedin.com/in/vincent-wc-wong/" target="_blank" rel="noopener noreferrer">
+          <img className="social-icon" src={linkedinImage} alt="Black and white LinkIn logo icon" />
+        </a>
+      </div>
     </div>
   </footer>
-)
+);
 
 export default Footer;
