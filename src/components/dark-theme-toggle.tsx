334b44bde4b33ce1f1f99af9954aa3702e4959b7
Vincent Wong <vincent@vwong.dev>
2020-09-12 13:04:23 +1000

    Remove direct manipulation of DOM

--- src/components/dark-theme-toggle.tsx
+++ src/components/dark-theme-toggle.tsx
@@ -1,33 +1,33 @@
-import React, { useState } from "react"
+import React, { useState } from 'react';
 
-import sunImage from "../images/sun.svg"
-import moonImage from "../images/moon.svg"
+import sunImage from '../images/sun.svg';
+import moonImage from '../images/moon.svg';
 
 const DarkThemeToggle = () => {
   const [isDarkTheme, setDarkTheme] = useState(false);
 
   return (
     <div id="dark-theme-button" className="link-container">
-      <img className="social-icon" src={moonImage} alt="Black and white moon icon"/>
-      <a className="underline" onClick={() => {
-        toggleDarkTheme(isDarkTheme) 
-        setDarkTheme(!isDarkTheme)
-      }}>Dark</a>
-    </div> 
-  )
-}
+      <img className="social-icon" src={isDarkTheme ? moonImage : sunImage} alt="Black and white moon icon" />
+      <a
+        className="underline"
+        onClick={() => {
+          toggleDarkTheme(isDarkTheme);
+          setDarkTheme(!isDarkTheme);
+        }}
+      >
+        {isDarkTheme ? 'Dark' : 'Light'}
+      </a>
+    </div>
+  );
+};
 
 const toggleDarkTheme = (isDarkTheme: boolean) => {
-  document.body.classList.toggle("dark");
-  document.getElementsByClassName("profile")[0].classList.toggle("dark");
-  for (let element of document.querySelectorAll(".card, a, .social-icon, .basic-card")) {
-    element.classList.toggle("dark");
+  document.body.classList.toggle('dark');
+  document.getElementsByClassName('profile')[0].classList.toggle('dark');
+  for (const element of document.querySelectorAll('.card, a, .social-icon, .basic-card')) {
+    element.classList.toggle('dark');
   }
+};
 
-  const darkThemeToggleButton = document.getElementById("dark-theme-button");
-  const darkThemeButtonText = darkThemeToggleButton.children[1];
-  darkThemeToggleButton.children[0].src = isDarkTheme ? moonImage : sunImage;
-  darkThemeButtonText.innerText = isDarkTheme ? "Dark" : "Light";
-}
-
-export default DarkThemeToggle
+export default DarkThemeToggle;
