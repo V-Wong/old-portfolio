539623678f2445e3bd0555944cc5acfbd9a7204b
Vincent Wong <vincent@vwong.dev>
2020-11-24 13:31:29 +1100

    Remove link underlines

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -14,12 +14,11 @@ const Header = () => (
       <nav className="links-container">
         <div className="link-container">
           <img className="social-icon" src={githubImage} alt="Black and white GitHub logo icon" />
-          <a className="underline" href="https://github.com/V-Wong" target="_blank" rel="noopener noreferrer">Github</a>
+          <a href="https://github.com/V-Wong" target="_blank" rel="noopener noreferrer">Github</a>
         </div>
         <div className="link-container">
           <img className="social-icon" src={resumeImage} alt="Black and white icon with silhouette of person" />
           <a
-            className="underline"
             href="https://drive.google.com/file/d/1mqRF7QdBDXfDD7TLQ3OuanALxHxd_D2f/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
@@ -29,7 +28,7 @@ const Header = () => (
         </div>
         <div className="link-container">
           <img className="social-icon" src={linkedinImage} alt="Black and white LinkIn logo icon" />
-          <a className="underline" href="https://www.linkedin.com/in/vincent-wc-wong/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/vincent-wc-wong/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         </div>
       </nav>
     </div>
